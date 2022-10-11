c9c80b51b1660640e4800a0060eb4d34fd18fb87
Karla Virgilio <karla98vl@gmail.com>
2022-10-11 12:25:05 -0500

    Actualización a archivo de BD para conexión sql server

--- db.php
+++ db.php
@@ -1,21 +1,35 @@
 <?php
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "test"; 
+$dbname = "nueva";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
+// Test db user
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} 
-$sql = "INSERT INTO registro (descripcion ) VALUES ( '{$_POST['descripcion']}');"; 
-
+} else {
+    echo "Conexión establecida.<br />";
+}
+$sql = "INSERT INTO registro (descripcion ) VALUES ( '{$_POST['descripcion']}');";
 if ($conn->multi_query($sql) === TRUE) {
   echo "New records created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error; 
 }
+$conn->close();*/
+//Conección a base SQL SERVER: Instalar ultima versiones de php agregar extensiones pdo, odbc para sql
+// y reinicar windows despues de agregar conf en el php.init
+$serverName = 'LAPTOP-SPVHJIUH\SQLEXPRESS';
+$connectionInfo = array( "Database"=>"DWH_Artigraf");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-$conn->close();
+if( $conn ) {
+    echo "Conexión establecida.<br />";
+}else{
+    echo "Conexión no se pudo establecer.<br />";
+    die( print_r( sqlsrv_errors(), true));
+}
+
 ?>
