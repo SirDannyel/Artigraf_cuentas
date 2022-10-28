e15150426c501dd9581b76fe1d05b6411489e7b6
acosvaz <ivettita_96@live.com.mx>
2022-10-28 14:36:30 -0500

    mov update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 
 return array(
-    'server' => 'LAPTOP-SPVHJIUH\SQLEXPRESS', // Karla
+    //'server' => 'LAPTOP-SPVHJIUH\SQLEXPRESS', // Karla
  //'server' => 'DESKTOP-092HCCI', // Ivette
  //'server' => 'DESKTOP-907DBP9\SQLEXPRESS', // Daniel
  'database' => 'DWH_Artigraf',
